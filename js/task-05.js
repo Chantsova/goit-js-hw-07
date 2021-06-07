055f793811a0c42e2a85785204ac80e1aad89d58
Chantsova Anastasia <77212568+Chantsova@users.noreply.github.com>
2021-06-07 20:34:51 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const refs = {
+    inputEl: document.querySelector('#name-input'),
+    outputEl: document.querySelector('#name-output'),
+}
+
+refs.inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if (event.currentTarget.value !== '') {
+    refs.outputEl.textContent = event.currentTarget.value;
+}
+    if (event.currentTarget.value === '') {
+    refs.outputEl.textContent = "Незнакомец";
+}
+}
