5b6b6193826c9d22f1d2377c387b074a1f13c31c
Chantsova Anastasia <77212568+Chantsova@users.noreply.github.com>
2021-06-10 16:13:09 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,45 @@
-//1.17.50 ot 02.06.21
+const refs = {
+    inputEl: document.querySelector('input'),
+    renderEl: document.querySelector('[data-action="render"]'),
+    destroyEl: document.querySelector('[data-action="destroy"]'),
+    boxesContainer: document.querySelector('#boxes'),
+};
+
+let collection = [];
+
+refs.renderEl.addEventListener('click', createBoxes);
+refs.destroyEl.addEventListener("click", destroyBoxes); 
+
+function createBoxes(amount) {
+    amount = refs.inputEl.value;
+    collection.length = amount; 
+    collection.fill(0, 0, amount);
+
+    let sizeDiv = 30;
+    
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }; 
+
+    const divCollection = collection.map((item, idx) => {
+        if(idx!==0) {
+            sizeDiv += 10;
+        };
+
+        const redColor = getRandomInt(0,255);
+        const greenColor = getRandomInt(0,255);
+        const blueColor = getRandomInt(0,255);
+
+        const divEl = `<div style="width: ${sizeDiv}px; height: ${sizeDiv}px; background-color: rgb(${redColor}, ${greenColor}, ${blueColor})"></div>`;
+        item === divEl;
+        return divEl;
+    });
+    refs.boxesContainer.insertAdjacentHTML("beforeend", divCollection.join(''));
+};
+
+function destroyBoxes() {
+    refs.inputEl.value = '';
+        while (refs.boxesContainer.lastElementChild) {
+            refs.boxesContainer.removeChild(refs.boxesContainer.lastElementChild);
+    };
+};
